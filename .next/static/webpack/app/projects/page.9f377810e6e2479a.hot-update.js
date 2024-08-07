bca8aeacdb8cf75c1c44f02551500644670c67fb
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2024-08-07 04:37:41 -0500

    added

---
+++ .next/static/webpack/app/projects/page.9f377810e6e2479a.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/projects/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C91860%5COneDrive%5CDocuments%5CGitHub%5CSimon-Game%5CNaved-Portfolio%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C91860%5COneDrive%5CDocuments%5CGitHub%5CSimon-Game%5CNaved-Portfolio%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/link.js */ \"(app-pages-browser)/./node_modules/next/dist/client/link.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1DJTNBJTVDVXNlcnMlNUM5MTg2MCU1Q09uZURyaXZlJTVDRG9jdW1lbnRzJTVDR2l0SHViJTVDU2ltb24tR2FtZSU1Q05hdmVkLVBvcnRmb2xpbyU1Q25vZGVfbW9kdWxlcyU1Q25leHQlNUNkaXN0JTVDY2xpZW50JTVDbGluay5qcyZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ODQ5NCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXDkxODYwXFxcXE9uZURyaXZlXFxcXERvY3VtZW50c1xcXFxHaXRIdWJcXFxcU2ltb24tR2FtZVxcXFxOYXZlZC1Qb3J0Zm9saW9cXFxcbm9kZV9tb2R1bGVzXFxcXG5leHRcXFxcZGlzdFxcXFxjbGllbnRcXFxcbGluay5qc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C91860%5COneDrive%5CDocuments%5CGitHub%5CSimon-Game%5CNaved-Portfolio%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false!\n"));
+
+/***/ })
+
+});
---
+++ app/about/_components/Experience.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Heading from "@/components/Heading";
+
+function Experience() {
+  return (
+    <div className="container mx-auto px-4">
+      <Heading as="h1" size="xl" className="mb-8 text-center">
+        Experience
+      </Heading>
+      <div className="grid gap-6 md:grid-cols-2">
+        <div className="prose prose-xl prose-slate prose-invert">
+          <div className="mb-2 font-bold">Xzect Labs</div>
+          <div className="text-sm font-thin text-yellow-500">Full Stack Developer Intern</div>
+          <div className="text-sm font-thin mb-2">2024 Jun - Aug (2 Months)</div>
+          <div className="">
+           Led the development of a full-stack project, including an AI bot, leveraging HTML, CSS, JavaScript, React, and Next.js to create dynamic and interactive web applications.
+           Implemented responsive design and modern user interfaces using Bootstrap and Tailwind, ensuring a seamless user experience across devices.
+           Contributed to open-source projects by managing API keys, collaborating on production-level code, and enhancing both functionality and security.
+           Worked closely with the team to integrate features, optimize performance, and improve user engagement through rigorous testing and iterative improvements.
+           Acquired hands-on experience in production environments, actively participating in code reviews and contributing to the enhancement of development processes.
+
+           </div>
+        </div>
+
+        <div className="prose prose-xl prose-slate prose-invert">
+          <div className="mb-2 font-bold">OctaNet Services Pvt Ltd.</div>
+          <div className="text-sm font-thin text-yellow-500">Web Developer Intern</div>
+          <div className="text-sm font-thin mb-2">2024 Jun - Aug</div>
+          <div className="">
+            Developed a responsive landing page and an interactive to-do list
+            using HTML, CSS, and JavaScript. Applied best practices for
+            cross-browser compatibility and user experience enhancements.
+            Collaborated with the design team to ensure visual and functional
+            integrity of web components. Optimized web performance to enhance
+            loading speeds and user engagement. Participated in code reviews and
+            contributed to the improvement of development processes.
+          </div>
+        </div>
+
+        <div className="prose prose-xl prose-slate prose-invert">
+          <div className="mb-2 font-bold">CodeClause</div>
+          <div className="text-sm font-thin text-yellow-500">Web Developer Intern</div>
+          <div className="text-sm font-thin mb-2">2023 Oct - Dec</div>
+          <div className="">
+            Developed a live language translation web app using HTML, CSS,
+            JavaScript, and a Translation API. Created and maintained websites
+            using core web technologies. Ensured web applications were
+            responsive and user-friendly. Collaborated with team members to
+            integrate new features and improve existing functionality.
+            Participated in testing and debugging processes to enhance app
+            performance and reliability.
+          </div>
+        </div>
+
+        <div className="prose prose-xl prose-slate prose-invert">
+          <div className="mb-2 font-bold">Bharat Intern</div>
+          <div className="text-sm font-thin text-yellow-500">Web Developer Intern</div>
+          <div className="text-sm font-thin mb-2">2023 Sep - Nov</div>
+          <div className="">
+            Developed a dynamic e-commerce website with a user-friendly
+            interface. Implemented secure payment gateways and enhanced site
+            performance using JavaScript optimizations. Collaborated with
+            back-end developers to integrate RESTful APIs for seamless data
+            handling. Conducted usability testing to ensure an intuitive user
+            experience. Maintained and updated website content, ensuring
+            compliance with current web standards.
+          </div>
+        </div>
+
+        <div className="prose prose-xl prose-slate prose-invert">
+          <div className="mb-2 font-bold">Oasis Infobyte</div>
+          <div className="text-sm font-thin text-yellow-500">Web Developer Intern</div>
+          <div className="text-sm font-thin mb-2">2023 Sep - Oct</div>
+          <div className="">
+            Developed a fully functional web-based calculator. Created a
+            temperature converter allowing users to convert between different
+            temperature scales using HTML, CSS, and JavaScript. Ensured both
+            applications were responsive and user-friendly. Implemented clean
+            and efficient code following best practices. Participated in the
+            testing and debugging processes to improve app performance and
+            accuracy.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
